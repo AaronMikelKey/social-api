cbb54a7300f88f38ea3cf55164bd4bef2cb51bf6
Aaron Key <aaronmikelkey@gmail.com>
2022-10-06 15:32:26 -0400

    Add user model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (email) {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          email
+        );
+      },
+      message: (props) => `${props.value} is not a valid email address.`,
+    },
+  },
+  thoughts: { type: Array },
+  friends: { type: Array },
+  friendCount: { type: Number },
+});
+
+UserSchema.virtual("friendCount").get(() => {
+  return this.friends.length;
+});
+
+export default UserSchema;
